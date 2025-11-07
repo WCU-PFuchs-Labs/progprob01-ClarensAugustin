412496e40ce56a3f3e1e7954e6b8c04d5e8017b0
ClarensAugustin <161627356+ClarensAugustin@users.noreply.github.com>
2025-11-07 15:57:19 -0500

    Update homework20_1.java

--- homework20_1.java
+++ homework20_1.java
@@ -15,16 +15,60 @@ output: head-->1-->2-->3-->null
 
 Please complete the following program to fullfil the function.
 */
+import java.util.Scanner;
+
 public class homework20_1
 {
    public static void main(String[] args)
    {
       //add your code here
-   
+      Scanner scanner = new Scanner(System.in);
+      LinkedList llist = new LinkedList();
+
+      System.out.println("Enter 5 integer numbers: ");
+
+      for (int i = 0; i <= 4; i++) {
+         int input = scanner.nextInt();
+
+         ListNode llistNode = new ListNode(input);
+         if (llist.head == null) {
+            llist.head = llistNode;
+         } else {
+            ListNode current = llist.head; // start at llist head
+            while (current.next != null) {
+               current = current.next;
+            }
+            current.next = llistNode;
+         }
+      }
+
+      System.out.println("Orginal list: ");
+      System.out.println(llist);
+ 
+      deleteDuplicates(llist);
+
+      // Print result after duplicate
+      System.out.println(llist);
+
+      scanner.close();
    }
    public static void deleteDuplicates(LinkedList llist)
    {
       //add your code here
+      if (llist.head == null) {
+         return;
+      }
+
+      ListNode current = llist.head;
+
+      while (current != null && current.next != null) {
+         if (current.value == current.next.value) {
+            current.next = current.next.next; // moves on to the next node if they are equal
+         } else {
+            current = current.next;
+         }
+      }
+      
    }
 
 }
@@ -33,7 +77,9 @@ class ListNode
 {
    int value;
    ListNode next;
-   ListNode(int v){value = v;}
+   ListNode(int v){
+      value = v;
+   }
 }
 
 class LinkedList
